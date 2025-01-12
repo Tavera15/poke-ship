561134dfae4b0a6e3389b9fb73d4eb563096cfa9
Tavera15 <joaquinramirez97@yahoo.com>
2025-01-12 17:42:59 -0600

    Able to select cards

--- src/Components/PokeCard.jsx
+++ src/Components/PokeCard.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { Card, Button, CardBody } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
 import "./PokeCard.css";
 
-function PokeCard({pokemon})
+function PokeCard({pokemon, handleSelect})
 {
+    const [isSelected, setSelected] = useState(false);
+
+    function onCardClick()
+    {
+        setSelected(!isSelected);
+        handleSelect(pokemon.id);
+    }
+
     return(
-        <div className="col-6 p-0 col-sm-3">
-            <Card className="p-2 m-0" style={{"border": "none", "backgroundColor": "transparent"}}>
-                <Card.Img className="poke-card" variant="top" src={pokemon.images.large} />
+        <div className="col-6 p-2 col-sm-3" onClick={(e) => onCardClick(pokemon.id)} role="button">
+            <Card className={`p-2 m-0 p-0 poke-card ${isSelected ? "poke-card-selected" : ""}`} >
+                <Card.Img className="" variant="top" src={pokemon.images.large} />
             </Card>
         </div>
     );
--- src/Pages/PokemonPage/PokemonPage.jsx
+++ src/Pages/PokemonPage/PokemonPage.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import PokeCard from "../../Components/PokeCard";
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
 function PokemonPage()
 {
     const [deck, setDeck] = useState([]);
     const [isDeckLoaded, setCardLoaded] = useState(false);
+    const [hand, setHand] = useState([]);
 
     useEffect(() => {
         if(!isDeckLoaded)
@@ -16,19 +18,52 @@ function PokemonPage()
         }
       }, [])
 
+    function handleSelect(cardId)
+    {
+        if(!hand.includes(cardId))
+        {
+            addToHand(cardId);
+        }
+        else
+        {
+            removeFromHand(cardId);
+        }
+    }
+
+    function addToHand(cardId)
+    {
+        setHand((prev) => [...prev, cardId]);
+    }
+
+    function removeFromHand(cardId)
+    {
+        const res = hand.filter((c) => c !== cardId);
+        setHand(res);
+    }
+
     return(
         <div className='mt-4 d-flex flex-column align-items-center'>
-            <h1 className='text-warning'>Pokemon</h1>
             {
                 !isDeckLoaded
                 ? <h1>Loading...</h1>
                 : <div className="d-flex row col-12 col-md-8">
+                    <h1 className='text-warning'>Pokemon</h1>
                     {
-                        deck.map((p, i) => {
-                            return <PokeCard pokemon={p} id={p.id} key={p.id} />
+                        deck.map((p) => {
+                            return <PokeCard pokemon={p} id={p.id} key={p.id} handleSelect={(cardId) => handleSelect(cardId)} />
                         })
                     }
                   </div>
+            }
+
+            {
+                hand.length < 1 
+                ? ""
+                : <Navbar bg="dark" data-bs-theme="dark" fixed="bottom">
+                    <Container className='d-flex justify-content-center p-2'>
+                        <Button variant='warning' className='btn-lg'>Checkout</Button>
+                    </Container>
+                 </Navbar>
             }
         </div>
     );
