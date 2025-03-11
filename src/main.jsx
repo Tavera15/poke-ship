3d96454469a240cf4fbbe2ef25a0e016c9c3097d
Tavera15 <joaquinramirez97@yahoo.com>
2025-03-11 15:35:45 -0500

    Updated main file to build on prod

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from "./store.jsx";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import App from './App.jsx'
-import store from "./store.jsx";
-import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
 )
